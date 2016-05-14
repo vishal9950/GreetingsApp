7683fa1614580a3ddfc170c27d60631cd8bf6eec
Vishal Vasnani <vishalvasnani123@gmail.com>
2016-05-14 23:25:18 +0530

    Add project files.

---
+++ Greetings/System/Windows/Visibility.cs
@@ -0,0 +1,10 @@
+﻿using Windows.UI.Xaml;
+
+namespace System.Windows
+{
+    internal class Visibility
+    {
+        public static global::Windows.UI.Xaml.Visibility Hidden { get; internal set; }
+        public static global::Windows.UI.Xaml.Visibility Visible { get; internal set; }
+    }
+}
